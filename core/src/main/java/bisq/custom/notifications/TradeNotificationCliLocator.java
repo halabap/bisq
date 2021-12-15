d5defc811a42bdf86b21a63c39744da91385d4cc
Pavel Halabala <halabap@email.cz>
2021-12-15 18:56:25 +0100

    Add custom trade handler

---
+++ core/src/main/java/bisq/custom/notifications/TradeNotificationCliLocator.java
@@ -0,0 +1,26 @@
+package bisq.custom.notifications;
+
+import javax.inject.Singleton;
+
+import java.io.File;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Singleton
+@Slf4j
+public class TradeNotificationCliLocator {
+
+    public String getTradeNotificationCliAbsolutePath() {
+        String tradeNotificationCli = System.getenv("TRADE_NOTIFICATION_CLI");
+        if (tradeNotificationCli == null) {
+            return null;
+        } else {
+            if (new File(tradeNotificationCli).exists()) {
+                return tradeNotificationCli;
+            } else {
+                log.error("Trade notification cli binary does not exist, file={}", tradeNotificationCli);
+                return null;
+            }
+        }
+    }
+}
